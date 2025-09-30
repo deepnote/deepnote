c144d646abc4b6d3713aaba0e4ac07e3771016fc
James Hobbs <bjhobbs06@gmail.com>
2025-09-30 17:54:43 +0100

    test: vitest, codecov, junit

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["<rootDir>/packages"],
-  testMatch: ["**/*.test.ts"],
-  collectCoverageFrom: ["packages/*/src/**/*.ts", "!**/*.test.ts"],
-  bail: true,
-  errorOnDeprecated: true,
-};
---
+++ vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    include: ["packages/**/*.test.ts"],
+    reporters: ["default", "junit"],
+    outputFile: {
+      junit: "./coverage/test-results.xml",
+    },
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html", "lcov"],
+      include: ["packages/*/src/**/*.ts"],
+      exclude: ["**/*.test.ts"],
+    },
+    bail: 1,
+  },
+});
+
