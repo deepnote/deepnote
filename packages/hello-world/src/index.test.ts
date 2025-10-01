a003c344de03761e4a0ee941a57c1ccd7ad50dee
Andy Jakubowski <hello@andyjakubowski.com>
2025-10-01 16:33:29 +0200

    refactor: disable globals in vitest
    Signed-off-by: Andy Jakubowski <hello@andyjakubowski.com>

--- packages/hello-world/src/index.test.ts
+++ packages/hello-world/src/index.test.ts
@@ -1,7 +1,8 @@
-import { helloWorld } from './index'
+import { describe, expect, it } from "vitest";
+import { helloWorld } from "./index";
 
-describe('helloWorld', () => {
-  it('should return Hello, World!', () => {
-    expect(helloWorld()).toBe('Hello, World!')
-  })
-})
+describe("helloWorld", () => {
+  it("should return Hello, World!", () => {
+    expect(helloWorld()).toBe("Hello, World!");
+  });
+});
