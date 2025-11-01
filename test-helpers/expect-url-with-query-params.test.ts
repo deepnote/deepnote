178aed06ef0b136a69b819b55ffc7b2bf8c148ed
jankuca <jan@jankuca.com>
2025-11-01 11:33:17 +0100

    add more tests for expect.urlWithQueryParams()

--- test-helpers/expect-url-with-query-params.test.ts
+++ test-helpers/expect-url-with-query-params.test.ts
@@ -32,4 +32,72 @@ describe('urlWithQueryParams', () => {
   it('should pass when the query params are the same but in different order', () => {
     expect('http://example.com/path?a=1&b=2').toEqual(expect.urlWithQueryParams('http://example.com/path?b=2&a=1'))
   })
+
+  it('should pass when there are no query params', () => {
+    expect('http://example.com/path').toEqual(expect.urlWithQueryParams('http://example.com/path'))
+  })
+
+  it('should fail when one URL has query params and the other does not', () => {
+    expect(() => {
+      expect('http://example.com/path?a=1').toEqual(expect.urlWithQueryParams('http://example.com/path'))
+    }).toThrowError()
+  })
+
+  it('should fail when expected URL has extra query params', () => {
+    expect(() => {
+      expect('http://example.com/path?a=1').toEqual(expect.urlWithQueryParams('http://example.com/path?a=1&b=2'))
+    }).toThrowError()
+  })
+
+  it('should fail when actual URL has extra query params', () => {
+    expect(() => {
+      expect('http://example.com/path?a=1&b=2').toEqual(expect.urlWithQueryParams('http://example.com/path?a=1'))
+    }).toThrowError()
+  })
+
+  it('should pass with URL-encoded values', () => {
+    expect('http://example.com/path?name=John%20Doe&email=test%40example.com').toEqual(
+      expect.urlWithQueryParams('http://example.com/path?email=test%40example.com&name=John%20Doe')
+    )
+  })
+
+  it('should pass with URL-encoded special characters', () => {
+    expect('http://example.com/path?query=hello%20world%21&filter=%3D%3D').toEqual(
+      expect.urlWithQueryParams('http://example.com/path?filter=%3D%3D&query=hello%20world%21')
+    )
+  })
+
+  it('should fail when URL-encoded values are different', () => {
+    expect(() => {
+      expect('http://example.com/path?name=John%20Doe').toEqual(
+        expect.urlWithQueryParams('http://example.com/path?name=Jane%20Doe')
+      )
+    }).toThrowError()
+  })
+
+  it('should pass when hash fragments are the same', () => {
+    expect('http://example.com/path?a=1&b=2#section').toEqual(
+      expect.urlWithQueryParams('http://example.com/path?b=2&a=1#section')
+    )
+  })
+
+  it('should fail when hash fragments are different', () => {
+    expect(() => {
+      expect('http://example.com/path?a=1&b=2#section1').toEqual(
+        expect.urlWithQueryParams('http://example.com/path?b=2&a=1#section2')
+      )
+    }).toThrowError()
+  })
+
+  it('should fail when one URL has a hash fragment and the other does not', () => {
+    expect(() => {
+      expect('http://example.com/path?a=1&b=2#section').toEqual(
+        expect.urlWithQueryParams('http://example.com/path?b=2&a=1')
+      )
+    }).toThrowError()
+  })
+
+  it('should pass with empty hash fragment', () => {
+    expect('http://example.com/path?a=1#').toEqual(expect.urlWithQueryParams('http://example.com/path?a=1#'))
+  })
 })
