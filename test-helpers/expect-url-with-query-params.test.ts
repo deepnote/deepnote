46bb64d78be9f47076161c43a783a9123336c42b
jankuca <jan@jankuca.com>
2025-11-01 11:17:07 +0100

    fix URL test helper test to check exact URL match

--- test-helpers/expect-url-with-query-params.test.ts
+++ test-helpers/expect-url-with-query-params.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest'
 
 describe('urlWithQueryParams', () => {
   it('should pass when the URLs are the same', () => {
-    expect('http://example.com/path?a=1&b=2').toEqual(expect.urlWithQueryParams('http://example.com/path?b=2&a=1'))
+    expect('http://example.com/path?a=1&b=2').toEqual(expect.urlWithQueryParams('http://example.com/path?a=1&b=2'))
   })
 
   it('should fail when the pathnames are different', () => {
