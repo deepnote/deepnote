0e1ac5a5c13c8117404840b187001c8f08d9bf29
tomas <tomas@kislan.sk>
2026-02-18 19:08:10 +0000

    Update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,11 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    server: {
+      deps: {
+        inline: ['@xterm/headless'],
+      },
+    },
     globals: false,
     include: ['**/*.test.ts'],
     reporters: ['default', 'junit'],
