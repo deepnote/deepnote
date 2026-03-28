c881194f9c9ea08d9c443a57a731b7b479331eea
tomas <tomas@kislan.sk>
2026-03-28 14:05:21 +0000

    Update test

--- packages/cli/src/commands/integrations/tests/edit-integration.materialize.test.ts
+++ packages/cli/src/commands/integrations/tests/edit-integration.materialize.test.ts
@@ -269,5 +269,11 @@ integrations:
             sshUser: tunnel-user
       "
     `)
+
+    const envContent = await readFile(envFilePath, 'utf-8')
+    expect(envContent).toMatchInlineSnapshot(`
+      "MZ_ID_001__PASSWORD=secret-pass
+      "
+    `)
   })
 })
